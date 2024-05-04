b05501f1a40c75236f73554371a5324c3c0aadb0
GERARD_DE_TENA <gerarddtm@gmail.com>
2024-05-04 21:13:01 +0200

    todolist

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../../Fp64-BobEsponja-ToDo-List/bobesponja-todo-list/src/App.jsx'
+import '../../Fp64-BobEsponja-ToDo-List/bobesponja-todo-list/src/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
