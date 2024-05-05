918250a67bfc945d516aa07ff2f29d6eae411fad
GerarddeTena <156508398+GerarddeTena@users.noreply.github.com>
2024-05-05 08:52:51 +0000

    Needs to fix background

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ToDoList from "./comps/ToDoList";
+
+const App = () => {
+    return (
+        <>
+           <ToDoList />
+        </>
+    )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../../Fp64-BobEsponja-ToDo-List/bobesponja-todo-list/src/App.jsx'
-import '../../Fp64-BobEsponja-ToDo-List/bobesponja-todo-list/src/index.css'
+import App from './App.jsx'
+import './Index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
